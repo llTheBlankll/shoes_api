c726f6d80a18c55d197a8e02ba0782a62128e017
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-08-08 19:10:32 +0800

    fix: update application properties and add new FeatureController
    - updated `spring.datasource.url` to point to a new database host
    - updated `spring.datasource.password` to use a secured password
    - added `server.port` configuration
    - added `springdoc.paths-to-match` configuration to enable API documentation
    - created new `FeatureController` class to handle feature-related API requests

---
+++ src/main/java/com/asharia/dss/controllers/FeatureController.java
@@ -0,0 +1,13 @@
+package com.***REMOVED***.dss.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/features")
+@Tag(name = "Feature", description = "Feature API")
+public class FeatureController {
+
+
+}
